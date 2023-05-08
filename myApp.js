7b3e51bfacbea834eceaf5051cf617ebf9d8e9b7
Evans Kipngetich <kiprotich.evans.kipngetich@gmail.com>
2023-05-08 12:32:34 +0300

    first commit

---
+++ myApp.js
@@ -0,0 +1,48 @@
+let express = require('express');
+let app = express();
+
+console.log("Hello World");
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(3000, function () {
+//   console.log("App Listening to port 3000");
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;

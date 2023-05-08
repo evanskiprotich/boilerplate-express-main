0d4fd3e8e7463f0105ae53ac83f8766848aa69b1
Evans Kipngetich <kiprotich.evans.kipngetich@gmail.com>
2023-05-08 12:55:29 +0300

    directory updates

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 // console.log("Hello World");
 app.get('/', (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(__dirname + '/views/index.html')
 })
 
 // app.listen(3000, function () {

af125a35da19bf7a9a52837b1ab66f58d8018ca0
Evans Kipngetich <kiprotich.evans.kipngetich@gmail.com>
2023-05-08 14:03:42 +0300

    checks

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+// Serve static files from the "public" directory
+app.use(express.static(__dirname + "/public"));
 // console.log("Hello World");
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -9,6 +11,14 @@ app.get('/', (req, res) => {
 // app.listen(3000, function () {
 //   console.log("App Listening to port 3000");
 // })
+
+;
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
 
 
 
